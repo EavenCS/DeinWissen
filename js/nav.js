f25b806674a713a67037c1e3a4458f1681a6c60e
EavenCS <eaven7juni@gmail.com>
2025-01-31 16:07:49 +0100

    enhance navigation bar with logo and mobile toggler

--- js/nav.js
+++ js/nav.js
@@ -2,10 +2,15 @@
 document.getElementById('nav').innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container">
-            <a class="navbar-brand" href="index.html">Quiz-App</a>
+            <a class="navbar-brand" href="index.html">
+                <img src="assets/logo.svg" alt="Quiz-App Logo" width="30" height="30" class="d-inline-block align-text-top">
+                Quiz-App
+            </a>
+
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
